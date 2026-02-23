fd039719f63f51efb0b255c22c0ab92905f369ff
tangerino4 <81923856+tangerino4@users.noreply.github.com>
2026-02-23 05:13:16 +0100

    Update made-with-dyad.tsx

--- src/components/made-with-dyad.tsx
+++ src/components/made-with-dyad.tsx
@@ -7,7 +7,7 @@ export const MadeWithDyad = () => {
         rel="noopener noreferrer"
         className="text-sm text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-200" 
       >
-        Hi world hfghfg hi f a
+        Hi world hfghfg hi f a a
       </a>
     </div>
   );
